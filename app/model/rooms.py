3381b829c318a59261a18aba9f18aea22b64acae
Aarush-Gupta15 <139454727+Aarush-Gupta15@users.noreply.github.com>
2023-11-16 09:36:19 +0530

    room alotment created

---
+++ app/model/rooms.py
@@ -0,0 +1,64 @@
+import mysql.connector  
+
+
+def getConnection():
+    #Create the connection object   
+    global myconn 
+    myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "admin",database = "BULIV")  
+ 
+    #creating the cursor object  
+    
+
+#inserting the values
+def allotRoom(studentId,roomType,fromDate,toDate):
+    getConnection()
+
+    freeRooms = getFreeRooms(roomType)
+    roomToAllocate = freeRooms[0]
+    roomIdToAllocate = roomToAllocate[0] 
+    
+    mysqlquery = "insert into ROOM_ALLOTMENT(STUDENT_ID,ROOM_ID,FROM_DATE,TO_DATE) VALUES (%s, %s, %s,%s)"
+    mycursor = myconn.cursor()
+    values = (studentId, roomIdToAllocate, fromDate, toDate)  
+    mycursor.execute(mysqlquery, values)
+    myconn.commit() 
+    print("alloted succesfully")
+    updateRoomAvilbility(roomIdToAllocate)
+
+
+def updateRoomAvilbility(roomId):
+    getConnection()
+    mysqlquery = "UPDATE ROOMS SET AVAILABILITY='BOOKED' WHERE ID=(?)"
+    mycursor = myconn.cursor()
+    mycursor.execute(mysqlquery,roomId)
+    myconn.commit() 
+    print("updated succesfully")
+
+
+def getFreeRooms(roomType):
+    getConnection()
+
+    mysqlquery = "SELECT * FROM ROOMS WHERE AVAILABILITY='FREE' AND ROOM_TYPE='" + roomType +"'"
+    mycursor = myconn.cursor()
+    mycursor.execute(mysqlquery)
+    allFreeRooms=mycursor.fetchall()
+    print(allFreeRooms)
+    return allFreeRooms
+
+
+def printstudents():
+    getConnection()
+
+    mysqlquery = "SELECT * FROM STUDENTS"
+    mycursor = myconn.cursor()
+    mycursor.execute(mysqlquery)
+    allstudent=mycursor.fetchall()
+    #print(allstudent)
+    return allstudent
+#printstudents()
+
+allotRoom(2,"DOUBLE","2022-05-22","2022=05-26")
+
+    
+
+    
--- sql/create.sql
+++ sql/create.sql
@@ -16,5 +16,25 @@ SECOND_NAME varchar(100),
 COURSE_ID INT(10),
 UNIQUE (MOBILE)
 );
+
+CREATE TABLE ROOMS(
+ID INT PRIMARY KEY auto_increment,
+BLOCK_NAME varchar(20) NOT NULL,
+ROOM_TYPE varchar(10)
+);
+
+ALTER TABLE ROOMS ADD AVAILABILITY varchar(20);
+
+
+CREATE TABLE ROOM_ALLOTMENT(
+ID INT PRIMARY KEY auto_increment,
+STUDENT_ID INT,
+ROOM_ID varchar(10),
+FROM_DATE DATE,
+TO_DATE DATE
+);
+
+
+
 show tables
 select * from students
